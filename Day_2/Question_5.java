98340d3d4b2f06d88acbc982d9fe5a8582ede85d
samratbarar <samratbarar7707@gmail.com>
2026-05-29 18:23:25 +0530

    Question 5 is completed

---
+++ Day_2/Question_5.java
@@ -0,0 +1,23 @@
+package summer_Assignment_25114FW001.Day_2;
+
+import java.util.*;
+
+// Write a program to Find sum of digits of a number
+
+public class Question_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.println("The sum of the digits of given number is " + sum);
+    }
+}
