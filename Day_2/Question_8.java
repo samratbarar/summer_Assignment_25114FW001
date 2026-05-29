3eb27cefa006decbb5faad894b452fe4e4e4d540
samratbarar <samratbarar7707@gmail.com>
2026-05-29 18:46:18 +0530

    Question 8 is completed

---
+++ Day_2/Question_8.java
@@ -0,0 +1,28 @@
+package summer_Assignment_25114FW001.Day_2;
+
+import java.util.*;
+
+// Write a program to Check whether a number is palindrome.
+
+public class Question_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number ");
+        int num = sc.nextInt();
+
+        int revNum = 0;
+        int checker = num;
+
+        while (num > 0) {
+            revNum = revNum*10 + num%10;
+            num /= 10;
+        }
+
+        if (checker == revNum) {
+            System.out.println("Given number is palindrome number ");
+        } else {
+            System.out.println("Given number is not palindrome number ");
+        }
+    }
+}
