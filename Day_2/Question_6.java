2e812f9521375017cc5d0e4c51a4549b2664dd5d
samratbarar <samratbarar7707@gmail.com>
2026-05-29 18:34:56 +0530

    Question 6 is completed

---
+++ Day_2/Question_6.java
@@ -0,0 +1,23 @@
+package summer_Assignment_25114FW001.Day_2;
+
+import java.util.*;
+
+// Write a program to Reverse a number.
+
+public class Question_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number ");
+        int num = sc.nextInt();
+
+        int revNum = 0;
+
+        while (num > 0) {
+            revNum = revNum*10 + num%10;
+            num /= 10;
+        }
+
+        System.out.println("The reverse of given number is " + revNum);     
+    }
+}
