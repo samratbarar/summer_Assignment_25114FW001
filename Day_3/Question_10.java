ee7cffec2152e2e1530c1ecd854b9b9c6bc02bdd
samratbarar <samratbarar7707@gmail.com>
2026-05-30 20:20:25 +0530

    Question 10 is completed

---
+++ Day_3/Question_10.java
@@ -0,0 +1,31 @@
+package summer_Assignment_25114FW001.Day_3;
+
+import java.util.*;
+
+// Write a program to Print prime numbers in a range
+
+public class Question_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of ineteger upto which prime numbers shoukd be printed ");
+        int range = sc.nextInt();
+
+        // as 0 and 1 is not primee hence we can exclude them
+        for (int i = 2; i <= range; i++) {
+
+            int count = 0;
+
+            for (int j = 2; j <= i/2; j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+
+            if (count == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
