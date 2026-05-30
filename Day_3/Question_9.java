f782c2de291437a85e30b07f40d4181f2f970f76
samratbarar <samratbarar7707@gmail.com>
2026-05-30 19:59:20 +0530

    Question 9 is completed

---
+++ Day_3/Question_9.java
@@ -0,0 +1,29 @@
+package summer_Assignment_25114FW001.Day_3;
+
+import java.util.*;
+
+// Write a program to Check whether a number is prime
+
+public class Question_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        for(int i = 2; i <= num/2; i++) {
+            if(num%i == 0) {
+                count++;
+                break;
+            }
+        }
+        
+        if (count == 1 || num == 0 || num == 1) {
+            System.out.println("Entered number is not prime");
+        }else {
+            System.out.println("Entered number is prime");
+        }
+    }
+}
