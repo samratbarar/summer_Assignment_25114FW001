da49a0d883545fa0782c169835269e5d72fdf0bb
samratbarar <samratbarar7707@gmail.com>
2026-05-31 19:50:27 +0530

    Question 15 is completed

---
+++ Day_4/Question_15.java
@@ -0,0 +1,34 @@
+package summer_Assignment_25114FW001.Day_4;
+
+import java.util.*;
+
+// Write a program to Check Armstrong number.
+
+public class Question_15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number ");
+        int num = sc.nextInt();
+
+        int checker = num, noOfDigit = 0, sum = 0;
+
+        while (num > 0) {
+            noOfDigit++;
+            num /= 10;
+        }
+
+        num = checker;
+
+        while (num > 0) {
+            sum += Math.pow(num%10 , noOfDigit);
+            num /= 10;
+        }
+
+        if (checker == sum) {
+            System.out.println("Given number is armstrong number ");
+        } else {
+            System.out.println("Given number is not armstrong number ");
+        }
+    }   
+}
