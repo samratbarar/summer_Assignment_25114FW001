059c3a283c07c87e12af9e9aa41d3a2e92ac892e
samratbarar <samratbarar7707@gmail.com>
2026-06-01 20:02:38 +0530

    Question 19 is completed

---
+++ Day_5/Question_19.java
@@ -0,0 +1,30 @@
+package summer_Assignment_25114FW001.Day_5;
+
+import java.util.*;
+
+// Write a program to Print factors of a number. 
+
+public class Question_19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+
+        if (num == 0) {
+            System.out.println("Given number is zero which have infinte fractors");
+            return;
+        } else {
+            if (num < 0) {
+                num = -1 * num;
+            } 
+            for (int i = 1; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    System.out.print(i + "," + -1*i + ",");
+                }
+            }
+
+            System.out.print(num + "," + -1*num);
+        }
+    }
+}
