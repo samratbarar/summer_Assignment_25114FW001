ac24a08e06e5368316fe4465e0a6a5e84a2f6ae3
samratbarar <samratbarar7707@gmail.com>
2026-06-02 19:49:20 +0530

    Question 21 is completed

---
+++ Day_6/Question_21.java
@@ -0,0 +1,22 @@
+package summer_Assignment_25114FW001.Day_6;
+
+import java.util.*;
+
+// Write a program to Convert decimal to binary. 
+
+public class Question_21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a decimal number ");
+        int deciNum = sc.nextInt(), binNum = 0, base = 1;
+
+        while (deciNum > 0) {
+            binNum += (deciNum%2) * base;
+            base *= 10;
+            deciNum /= 2; 
+        }
+
+        System.out.println("Conversion of given decimal number into binary is " + binNum);
+    }
+}
