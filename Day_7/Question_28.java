b771c5978ada0d6911d387233e5c15c1ecf01ede
samratbarar <samratbarar7707@gmail.com>
2026-06-03 23:59:08 +0530

    small improvement

--- Day_7/Question_28.java
+++ Day_7/Question_28.java
@@ -4,15 +4,12 @@ import java.util.*;
 
 // Write a program to Recursive reverse number. 
 public class Question_28 {
-    public static void revNum(int n, int rev) {
+    public static int revNum(int n, int rev) {
         if (n == 0) {
-            rev /= 10;
-            System.out.println("The reverse of given number is " + rev);
-            return;
+            return rev;
         }
 
-        rev += (n%10);
-        revNum(n/10, rev*10);
+        return revNum(n / 10, rev * 10 + (n % 10));
     }
 
     public static void main(String[] args) {
@@ -21,6 +18,6 @@ public class Question_28 {
         System.out.println("Enter a number ");
         int num = sc.nextInt();
 
-        revNum(num, 0);
+        System.out.println("The reverse of given number is " + revNum(num, 0));
     }
 }
