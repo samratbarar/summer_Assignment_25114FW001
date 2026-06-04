8bd43fd493e814434d7ebc393d31d36627f29764
samratbarar <samratbarar7707@gmail.com>
2026-06-04 20:25:32 +0530

    Question 29 is completed

---
+++ Day_8/Question_29.java
@@ -0,0 +1,22 @@
+package summer_Assignment_25114FW001.Day_8;
+
+import java.util.*;
+
+// Write a program to Print half pyramid pattern. 
+
+public class Question_29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of half pyramid ");
+        int height = sc.nextInt();
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
