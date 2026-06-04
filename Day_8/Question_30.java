66d20518492573a72e3c5792b49c0097fd2981de
samratbarar <samratbarar7707@gmail.com>
2026-06-04 20:29:44 +0530

    Question 30 is completed

---
+++ Day_8/Question_30.java
@@ -0,0 +1,29 @@
+package summer_Assignment_25114FW001.Day_8;
+
+import java.util.Scanner;
+
+/*
+Write a program to Print number triangle. 
+1 
+12 
+123 
+1234 
+12345 
+*/
+
+public class Question_30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of half pyramid ");
+        int height = sc.nextInt();
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
