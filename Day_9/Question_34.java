6724c4ac9c2d67537754211e4d188ea8429713c6
samratbarar <samratbarar7707@gmail.com>
2026-06-05 13:10:16 +0530

    Question 34 is completed

---
+++ Day_9/Question_34.java
@@ -0,0 +1,31 @@
+package summer_Assignment_25114FW001.Day_9;
+
+import java.util.*;
+
+/*
+Write a program to Print reverse number 
+triangle. 
+ 
+12345 
+1234 
+123 
+12 
+1 
+*/
+
+public class Question_34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of half pyramid with repeated number ");
+        int height = sc.nextInt();
+
+        for (int i = height; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
