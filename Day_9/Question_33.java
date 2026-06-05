469a2a75b89f0f57f31dc0c812fd74e315b8e1b3
samratbarar <samratbarar7707@gmail.com>
2026-06-05 13:08:03 +0530

    Question 33 is completed

---
+++ Day_9/Question_33.java
@@ -0,0 +1,29 @@
+package summer_Assignment_25114FW001.Day_9;
+
+import java.util.*;
+
+/*
+Write a program to Print reverse star pattern. 
+***** 
+**** 
+*** 
+** 
+* 
+*/
+
+public class Question_33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of half pyramid with repeated number ");
+        int height = sc.nextInt();
+
+        for (int i = height; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
