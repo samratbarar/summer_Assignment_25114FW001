f26087f2df37b89d3a8a7c554a02c72883f61d1c
samratbarar <samratbarar7707@gmail.com>
2026-06-06 17:44:34 +0530

    Question 40 is completed

---
+++ Day_10/Question_40.java
@@ -0,0 +1,47 @@
+package summer_Assignment_25114FW001.Day_10;
+
+import java.util.*;
+
+/*
+Write a program to Print character pyramid.
+    A 
+   ABA 
+  ABCBA 
+ ABCDCBA 
+ABCDEDCBA 
+*/
+
+public class Question_40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of character pyramid ");
+        int height = sc.nextInt();
+
+        char letter;
+
+
+        for (int i = 1; i <= height; i++) {
+            // for spaces 
+            for (int j = 1; j <= height-i; j++) {
+                System.out.print(" ");
+            }
+
+            letter = 'A';
+
+            // numbers of half pyramid
+            for (int j = 1; j <= i; j++) {
+                System.out.print(letter++);
+            }
+
+            letter--;
+            
+            // numbers of second half
+            for (int j = i-1; j >= 1; j--) {
+                System.out.print(--letter);
+            }
+            
+            System.out.println();
+        }
+    }
+}
