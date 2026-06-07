ae26e41f3b1d44d2ceefd547eae68522054cc151
samratbarar <samratbarar7707@gmail.com>
2026-06-07 12:18:24 +0530

    Question 43 is completed

---
+++ Day_11/Question_43.java
@@ -0,0 +1,29 @@
+package summer_Assignment_25114FW001.Day_11;
+
+import java.util.*;
+
+// Write a program to Write function to check prime.
+
+public class Question_43 {
+    public static void primeNUMChecker(int n) {
+        if (n == 0 || n == 1 || n < 0) {
+            System.out.println("Given number is not prime");
+            return;
+        }
+
+        for (int i = 2; i*i <= n; i++) {
+            if (n%i == 0) {
+                System.out.println("Given number is not prime");
+                return;
+            }
+        }
+
+        System.out.println("The given is prime");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        primeNUMChecker(sc.nextInt());
+    }
+}
