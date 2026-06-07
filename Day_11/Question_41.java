c73a1c9aa898d0c3ec12bb77b05f8c8973eaf8e3
samratbarar <samratbarar7707@gmail.com>
2026-06-07 11:33:45 +0530

    Question 41 is completed

---
+++ Day_11/Question_41.java
@@ -0,0 +1,17 @@
+package summer_Assignment_25114FW001.Day_11;
+
+import java.util.Scanner;
+
+// Write a program to Write function to find sum of two numbers.
+
+public class Question_41 {
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two numbers");
+        System.out.println("The sum of two numbers is " + sum(sc.nextInt(), sc.nextInt()));
+    }
+}
